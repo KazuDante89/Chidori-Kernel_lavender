5e34bdc75da7d40148648cb13274562823c4f8e7
Arieffst <kuntulireng750@gmail.com>
2021-02-15 13:12:01 +0700

    compile.sh

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ export KBUILD_BUILD_HOST=#FstProject
 export PATH="$home/proton-clang/bin:$PATH"
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig CC=clang-12
-    make -j$(nproc) O=out ARCH=arm64 CC=clang-12 \
+    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig CC=clang
+    make -j$(nproc) O=out ARCH=arm64 CC=clang \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
