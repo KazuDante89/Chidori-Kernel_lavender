9e39c0f955245f5bfa670690ed2fa790a26c20c2
Arieffst <kuntulireng750@gmail.com>
2021-02-15 14:12:48 +0700

    compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export KBUILD_BUILD_USER=Arieffst
-export KBUILD_BUILD_HOST=#FstProject
+export KBUILD_BUILD_HOST=#fstproject
 export PATH="$home/proton-clang/bin:$PATH"
 # Compile plox
 function compile() {
