9b03804ba868500d55f66ac9a6d6b7abb1b2de51
melles1991 <melles.android@gmail.com>
2021-03-19 21:35:35 +0200

    defconfig: lavender: Regenerate
    Signed-off-by: melles1991 <melles.android@gmail.com>

--- build.sh
+++ build.sh
@@ -1,35 +1,105 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# Android Kernel Build Script
 
-export KERNELNAME=Phoenix
+echo -e " "
+echo -e " "
+echo -e "$blue░▐█▀█░▐█░▐█░▐██░▐█▀█▄▒▐█▀▀█▌▒▐█▀▀▄░▐██"
+echo -e "░▐█──░▐████─░█▌░▐█▌▐█▒▐█▄▒█▌▒▐█▒▐█─░█▌"
+echo -e "░▐█▄█░▐█░▐█░▐██░▐█▄█▀▒▐██▄█▌▒▐█▀▄▄░▐██$nocol"
+echo -e " "
 
-export LOCALVERSION=S1.4
+#Set Color
+blue='\033[0;34m'
+grn='\033[0;32m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+txtbld=$(tput bold)
+txtrst=$(tput sgr0)      
 
-export KBUILD_BUILD_USER=Arieffst
+# Main environtment
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG=lavender-oldcam_defconfig
 
-export KBUILD_BUILD_HOST=droneci
+# Export
+export ARCH=arm64
+export CROSS_COMPILE
+export CROSS_COMPILE_ARM32
+export KBUILD_BUILD_USER=melles1991
+export KBUILD_BUILD_HOST=CraftRom-build
 
-export TOOLCHAIN=clang
 
-export DEVICES=lavender
+echo -e "${txtbld}Config:${txtrst} $CONFIG"
+echo -e "${txtbld}ARCH:${txtrst} $ARCH"
+echo -e "${txtbld}Username:${txtrst} $KBUILD_BUILD_USER"
+make mrproper
+if [  -d "./out/" ]; then
+        rm -rf ./out/
+fi
 
-source helper
+# Main Staff
 
-gen_toolchain
+clang_bin="$HOME/toolchains/proton-clang/bin"
+gcc_prefix64="aarch64-linux-gnu-"
+gcc_prefix32="arm-linux-gnueabi-"
+CROSS_COMPILE="aarch64-linux-gnu-"
+CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
 
-send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} ${DEVICES}..."
+_ksetup_old_path="$PATH"
+export PATH="$clang_bin:$PATH"
 
-START=$(date +"%s")
+# Build start
+echo -e "$blue    \nMake DefConfig\n $nocol"
+make	O=out $CONFIG
+echo -e "$blue    \nBuild kernel\n $nocol"
+make	\
+	O=out \
+	ARCH=arm64 \
+	CC=clang \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	STRIP=llvm-strip \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	-j`nproc --all`
 
-for i in ${DEVICES//,/ }
-do
+if ! [ -a $KERN_IMG ]; then
+    echo -e "$red \nKernel Compilation failed! Fix the errors!\n $nocol"
+fi
 
-	build ${i} -oldcam
-	build ${i} -newcam
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
 
+# For MIUI Build
+# Credit Adek Maulana <adek@techdro.id>
+OUTDIR="$KERNEL_DIR/out/"
+VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
+
+STRIP="$HOME/toolchains/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/toolchains/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+            sed -e 's/gcc/strip/')"
+for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
+    "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
+    "${OUTDIR}"/scripts/sign-file sha512 \
+            "${OUTDIR}/certs/signing_key.pem" \
+            "${OUTDIR}/certs/signing_key.x509" \
+            "${MODULES}"
+    find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
 done
+cd libufdt/src && python2 mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
+echo -e "$grn    \n(i) Done moving modules\n $nocol"
 
-END=$(date +"%s")
-
-DIFF=$(( END - START ))
-
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version: $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+cd $ZIP_DIR
+cp $KERN_IMG zImage
+cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
+make normal &>/dev/null
+echo -e "$blue    \nFlashable zip generated under $yellow$ZIP_DIR.\n $nocol"
+cd ..
+# Build end
